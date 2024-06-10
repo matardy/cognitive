9a1866bf755bd666331f6cf4fe4e0d50bbc94d18
matardy <gutemberg.mendoza@epn.edu.ec>
2024-06-10 16:22:51 -0500

    update: add sns

---
+++ app/tools/aws_tools.py
@@ -0,0 +1,23 @@
+import boto3 
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+
+TOPIC_ARN = os.getenv('TOPIC_ARN') 
+
+def publish_message(subject, message):
+    sns_client = boto3.client('sns', region_name = 'us-east-1')
+
+    response = sns_client.publish(
+        TopicArn = TOPIC_ARN,
+        Message = message,
+        Subject = subject
+    )
+
+    print("Message Published succesfully")
+    return response
+
+if __name__ == "__main__":
+    subject = 'python test'
+    message = 'this is a test from a python script'
+    publish_message(subject=subject, message=message)

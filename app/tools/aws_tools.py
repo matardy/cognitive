c066da2c7eec78e855bbbf0d5db8f0d80ed35b29
matardy <gutemberg.mendoza@epn.edu.ec>
2024-06-10 16:38:07 -0500

    update: add sns

--- app/tools/aws_tools.py
+++ app/tools/aws_tools.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os 
 from botocore import UNSIGNED
 from botocore.client import Config
+import requests
 load_dotenv()
 
 TOPIC_ARN = os.getenv('TOPIC_ARN') 
@@ -19,7 +20,23 @@ def publish_message(subject, message):
     print("Message Published succesfully")
     return response
 
+def publicar_mensaje(subject, message, topic_arn=TOPIC_ARN):
+    url = "https://sns.us-east-1.amazonaws.com/"
+    data = {
+        "Action": "Publish",
+        "TopicArn": topic_arn,
+        "Message": message,
+        "Subject": subject,
+        "Version": "2010-03-31"
+    }
+    response = requests.post(url, data=data)
+    if response.status_code == 200:
+        print('Mensaje publicado:', response.text)
+    else:
+        print('Error al publicar mensaje:', response.text)
+
 if __name__ == "__main__":
     subject = 'python test'
     message = 'this is a test from a python script'
     publish_message(subject=subject, message=message)
+    publicar_mensaje(subject=subject, message=message)
